f1e9bbe1eaacbe5c2579dc379653e625287310fa
Dhanush <dhanu2kamrthi@gmail.com>
2021-10-28 08:48:45 +0530

    my message

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var name = document.getElementsByClassName('bgcolor')
+setInterval(function(){
+   document.body.style.backgroundColor='violet'
+},2000)
+
+
+setInterval(function(){
+   var para = document.body.style.backgroundColor='violet'
+},2000)
